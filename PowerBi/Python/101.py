92bfd8d6072d19cf92cf694ce1ed05bb1d36237d
Raj <raj.m.rao@gmail.com>
2022-02-23 21:26:51 -0700

    Update 101.py

--- PowerBi/Python/101.py
+++ PowerBi/Python/101.py
@@ -1,11 +1,9 @@
-# Sample code showing how to call PowerBi rest api using python.
-# Uses a file called "env.config", that provides credential info.
-# A very simple script that demonstrates how to use MSAL to call PBI-Rest API
-#
-# Sample env.config
 """
+A simple script that demonstrates calling PowerBI rest api using MSAL
+
+Sample env.config used in line 27
 {
-    "authorityUrl": "https://login.microsoftonline.com/{organization-ad-id-here}",
+    "authorityUrl": "https://login.microsoftonline.com/{organization-ad-id}",
     "client_id": "{client-id-guid-here}",
     "scope": ["https://analysis.windows.net/powerbi/api/.default"],
     "secret": "{client-secret-here}",
@@ -18,40 +16,43 @@ import json
 import logging
 
 import requests
-import msal     #pip install msal
+# pip install msal
+import msal
 
-# logging.basicConfig(level=logging.DEBUG)  # Enable DEBUG log for entire script
-# logging.getLogger("msal").setLevel(logging.INFO)  # Optionally disable MSAL DEBUG logs
+# Enable DEBUG log for entire script
+# logging.basicConfig(level=logging.DEBUG)
+# Optionally disable MSAL DEBUG logs
+# logging.getLogger("msal").setLevel(logging.INFO)
 
-config = json.load(open('env.config'))
+config = json.load(open('env.config', encoding="utf-8"))
 
 app = msal.ConfidentialClientApplication(
     config["client_id"], authority=config["authorityUrl"],
     client_credential=config["secret"]
 )
 
-result = None
-
 result = app.acquire_token_silent(config["scope"], account=None)
 
 if not result:
-    logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+    logging.info("No suitable token exists in cache. \
+                 Let's get a new one from AAD.")
     result = app.acquire_token_for_client(scopes=config["scope"])
 
 groupId = config["group_id"]
 datasetId = config["dataset_id"]
 if "access_token" in result:
-    print ("access_token retrieved!")
+    print("access_token retrieved!")
 
-    
     response = requests.get(
-        f"https://api.powerbi.com/v1.0/myorg/groups/{groupId}/datasets/{datasetId}/refreshes",
+        f"https://api.powerbi.com/v1.0/myorg/groups/{groupId}/datasets/\
+{datasetId}/refreshes",
         headers={'Authorization': 'Bearer ' + result['access_token']})
     response.raise_for_status()
     jsonData = response.json()
-    print("API call result: %s" % json.dumps(jsonData, indent=2))
+    print(f"API call result: {json.dumps(jsonData, indent=2)}")
 
 else:
     print(result.get("error"))
     print(result.get("error_description"))
-    print(result.get("correlation_id"))  # You may need this when reporting a bug
+    # You may need this when reporting a bug
+    print(result.get("correlation_id"))
