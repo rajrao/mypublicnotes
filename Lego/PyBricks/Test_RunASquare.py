07f2bc77fcbebb43132bd92a0dd104ac7d97ac6d
Raj <raj.m.rao@gmail.com>
2023-02-20 21:31:36 -0700

    Update Test_RunASquare.py

--- Lego/PyBricks/Test_RunASquare.py
+++ Lego/PyBricks/Test_RunASquare.py
@@ -8,6 +8,9 @@ from pybricks.tools import wait, StopWatch
 
 hub = InventorHub()
 
+#left_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+#right_motor = Motor(Port.A,Direction.CLOCKWISE)
+
 left_motor = Motor(Port.B, Direction.CLOCKWISE)
 right_motor = Motor(Port.A,Direction.COUNTERCLOCKWISE)
 
@@ -16,13 +19,26 @@ drive_base = DriveBase(left_motor,
     wheel_diameter = 56,
     axle_track= 80)
 
+turn_angle = -90
+length = 355.5
+width = 355.5
+
 for i in range(1):
-    drive_base.straight(177.5)
-    drive_base.turn(-90)
-    drive_base.straight(355.5)
-    drive_base.turn(-90)
-    drive_base.straight(355.5)
-    drive_base.turn(-90)
-    drive_base.straight(355.5)
-    drive_base.turn(-90)
-    drive_base.straight(177.5)
+    drive_base.straight(length/2.0)
+    print(f"{drive_base.distance()} {drive_base.angle()}")
+    
+    drive_base.turn(turn_angle)
+    drive_base.straight(width)
+    print(f"{drive_base.distance()} {drive_base.angle()}")
+
+    drive_base.turn(turn_angle)
+    drive_base.straight(length)
+    print(f"{drive_base.distance()} {drive_base.angle()}")
+
+    drive_base.turn(turn_angle)
+    drive_base.straight(width)
+    print(f"{drive_base.distance()} {drive_base.angle()}")
+
+    drive_base.turn(turn_angle)
+    drive_base.straight(length/2.0)
+    print(f"{drive_base.distance()} {drive_base.angle()}")
