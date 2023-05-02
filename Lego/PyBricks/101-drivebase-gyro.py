b211b0752cd4e24e089d38b0736d4dab61b32086
Raj <raj.m.rao@gmail.com>
2023-05-02 16:14:37 -0600

    Create 101-drivebase-gyro.py

---
+++ Lego/PyBricks/101-drivebase-gyro.py
@@ -0,0 +1,25 @@
+#https://docs.pybricks.com/en/latest/robotics.html#pybricks.robotics.GyroDriveBase
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+
+# Initialize both motors. In this example, the motor on the
+# left must turn counterclockwise to make the robot go forward.
+left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.B)
+
+# Initialize the drive base. In this example, the wheel diameter is 56mm.
+# The distance between the two wheel-ground contact points is 112mm.
+drive_base = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=112)
+
+# Drive forward by 500mm (half a meter).
+drive_base.straight(500)
+
+# Turn around clockwise by 180 degrees.
+drive_base.turn(180)
+
+# Drive forward again to get back to the start.
+drive_base.straight(500)
+
+# Turn around counterclockwise.
+drive_base.turn(-180)
